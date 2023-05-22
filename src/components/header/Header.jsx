3109ca9e40bce30fadd730b1da081b3fffbe4c16
unknown <pusurmankulovdami10@gmail.com>
2023-05-22 13:59:18 +0600

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Haader.module.scss'
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/registartionForm/RegistartionForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './RegistartionForm.module.scss'
+
+const RegistartionForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RegistartionForm;
